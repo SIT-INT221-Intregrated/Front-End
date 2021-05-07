fb11512ec692de2d7ed61112672bbd64c3c4f434
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-05-07 21:26:55 +0700

    [Update]Product & Image Push to BE

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="add max-w-lg mx-auto py-4">
-    <hi>Add New Product</hi>
+    <h1>Add New Product</h1>
     <form @submit.prevent="submitForm">
       <label for="name" class="leading-7 text-sm">Product Name</label>
       <input
@@ -11,6 +11,7 @@
         placeholder=""
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
       />
+
       <label for="price" class="leading-7 text-sm">Price</label>
       <input
         type="text"
@@ -20,63 +21,87 @@
         placeholder="฿"
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
       />
+
       <label for="description" class="leading-7 text-sm">Description</label>
       <textarea
         type="text"
         id="description"
         name="description"
-        v-model="description"
+        v-model="des"
         placeholder="..."
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out resize-none"
       />
-      <label for="brand" class="leading-7 text-sm"
-        >Brand:
-      </label>
-      <select
-        name="brand"
-        id="brand"
-        class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-16 w-40"
-      >
-        <option class="hidden" value="Select">Select...</option>
-        <option
-          v-for="brand in brands"
-          :key="brand.brandid"
-          :value="brand.brandname"
-        >
-          {{ brand.brandname }}
-        </option>
-      </select>
-      <label for="date" class="leading-7 text-sm ml-2.5">Update Date: </label>
+
+      <label for="date" class="leading-7 text-sm">Update Date: </label>
       <input
         type="date"
         id="date"
         name="date"
         v-model="date"
-        placeholder="$"
+        placeholder=""
         class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out max-w-max"
       />
+      <br />
+      <label for="brand" class="leading-7 text-sm">Brand: </label>
+      <div class="grid grid-cols-4">
+        <div
+          v-for="b in brands"
+          :key="b.brandid"
+          :value="b.brandid"
+          class="flex"
+        >
+          <input
+            type="radio"
+            v-model="brand"
+            id="brand"
+            name="brand"
+            :value="b.brandid"
+            class=""
+          />&nbsp;
+          {{ b.brandname }}
+        </div>
+      </div>
 
       <label for="color" class="leading-7 text-sm">Color: </label>
       <div class="grid grid-cols-3">
         <div
-          v-for="color in colors"
-          :key="color.colorid"
-          :value="color.hexcode"
+          v-for="c in colors"
+          :key="c.colorid"
+          :value="c.hexcode"
           class="flex"
         >
-          <input type="checkbox" id="color" name="color" :value="color.colorid" class=""/>&nbsp;
-          <div class="select-none border-black border-2 w-5 h-5 rounded-full" :style="{ 'background-color': color.hexcode }"></div>
-          &nbsp;{{ color.colorname }}
+          <input
+            type="checkbox"
+            v-model="color"
+            id="color"
+            name="color"
+            :value="c.colorid"
+            class=""
+          />&nbsp;
+          <div
+            class="select-none border-black border-2 w-5 h-5 rounded-full"
+            :style="{ 'background-color': c.hexcode }"
+          ></div>
+          &nbsp;{{ c.colorname }}
         </div>
       </div>
+
       <label for="image" class="leading-7 text-sm">Image: </label>
-      <input type="file" id="image" multiple class="" value="" />
-      <br>
-      <div class="flex justify-end">
-      <click-button
-        class="ButtonSubmit focus:outline-none bg-yellow-500 rounded-full p-1.5"
-        label="Submit"
+      <input
+        type="file"
+        id="image"
+        class="p-1 focus:outline-none rounded-md bg-gray-50"
+        accept="image/png,image/jpeg"
+        ref="image"
+        @change="uploadImage"
+        multiple
       />
+      <br />
+      <div class="flex justify-end">
+        <click-button
+          class="ButtonSubmit focus:outline-none bg-yellow-500 rounded-full p-1.5"
+          label="Submit"
+        />
       </div>
     </form>
   </div>
@@ -84,6 +109,7 @@
 
 <script>
 import ClickButton from "../components/ClickButton.vue";
+// import axios from 'axios';
 export default {
   components: {
     ClickButton,
@@ -93,16 +119,18 @@ export default {
       brandUrl: "http://localhost:3000/brands",
       colorUrl: "http://localhost:3000/colors",
       productUrl: "http://localhost:3000/products",
+      addProducts: "http://localhost:3000/addProducts",
+      UploadImage: "http://localhost:3000/UploadImage",
+      addProductColors: "http://localhost:3000/addProductColors",
       brands: [],
       colors: [],
-      result: [],
-      name: '',
-      price: '',
-      des: '',
-      brand: '',
-      date: '',
-      image: '',
-      selectedColor: [],
+      name: "",
+      price: "",
+      des: "",
+      brand: "",
+      date: "",
+      image: null,
+      color: [],
       invalidName: false,
       invalidPrice: false,
       invalidDes: false,
@@ -110,64 +138,105 @@ export default {
       invalidDate: false,
       invalidImage: false,
       invalidColor: false,
-      
     };
   },
   methods: {
-    submitForm(){
+    submitForm() {
       this.invalidName = this.name === "" ? true : false;
-      this.invalidPrice = this.price === "" ? true : false;
+      this.invalidPrice = this.price === 0 ? true : false;
       this.invalidDes = this.des === "" ? true : false;
       this.invalidBrand = this.brand === "" ? true : false;
       this.invalidDate = this.date === "" ? true : false;
-      this.invalidImage = this.image === "" ? true : false;
-      this.invalidColor = this.selectedColor === [] ? true : false;
+      this.invalidImage = this.image === null ? true : false;
+      this.invalidColor = this.color.length === 0 ? true : false;
 
-      console.log(`name: ${this.name}`)
-      console.log(`invalid name: ${this.invalidName}`)
-      console.log(`price: ${this.price}`)
-      console.log(`invalid price: ${this.invalidPrice}`)
-      console.log(`des: ${this.des}`)
-      console.log(`invalid des: ${this.invalidDes}`)
-      console.log(`brand: ${this.brand}`)
-      console.log(`invalid brand: ${this.invalidBrand}`)
-      console.log(`date: ${this.date}`)
-      console.log(`invalid date: ${this.invalidDate}`)
-      console.log(`image: ${this.image}`)
-      console.log(`invalid image: ${this.invalidImage}`)
-      console.log(`color: ${this.selectedColor}`)
-      console.log(`invalid color: ${this.invalidColor}`)
-
-      if(this.name !== "" && this.price !== "" && this.des !== "" && this.brand !== "" && this.date !== "" && this.image !== "" && this.color !== ""){
+      if (
+        this.name !== "" &&
+        this.price !== "" &&
+        this.des !== "" &&
+        this.brand !== "" &&
+        this.date !== "" &&
+        this.image !== null &&
+        this.color.length !== 0
+      ) {
         this.addProduct({
           name: this.name,
           price: this.price,
           des: this.des,
           brand: this.brand,
           date: this.date,
-          image: this.image,
-          color: this.color
+          image: this.image.name,
         });
+        // this.addProductColor(this.color)
       }
     },
-    // async addProduct(newProduct){
-    //   try {
-    //     const res = await fetch(this.productUrl, {
-    //       method: "POST",
-    //       headers: {
-    //         "content-type": "application/json"
-    //       },
-    //       body: JSON.stringify({
-            
-    //       })
-    //     });
-    //     const data = await res.json();
-    //     this.result = [...this.result, data];
-    //     location.reload();
-    //   }catch (error) {
-    //     console.log(`Cannot add product!: ${error}`);
-    //   }
-    // },
+    async addProduct(newProduct) {
+      console.log("Add Ja!!!!!!!");
+      let formData = new FormData();
+      var fullPath = document.getElementById("image").value;
+      if (fullPath) {
+        var startIndex =
+          fullPath.indexOf("\\") >= 0
+            ? fullPath.lastIndexOf("\\")
+            : fullPath.lastIndexOf("/");
+        var filename = fullPath.substring(startIndex);
+        if (filename.indexOf("\\") === 0 || filename.indexOf("/") === 0) {
+          filename = filename.substring(1);
+        }
+      }
+      let pro = {
+        productname: newProduct.name,
+        productdescription: newProduct.des,
+        price: newProduct.price,
+        saledate: newProduct.date,
+        brands_brandid: newProduct.brand,
+        images: newProduct.image,
+      };
+
+      let productJson = JSON.stringify(pro);
+      console.log(productJson);
+      fetch(`${this.addProducts}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: productJson,
+      });
+
+      console.log(this.image);
+      console.log(this.image.name);
+      formData.append("file", this.image,this.image.name);
+
+      fetch(`${this.UploadImage}`, {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+        body: formData,
+      });
+    },
+    async addProductColor(color) {
+      /* Error Cannot Add */
+      console.log("Product Color");
+      let co = { color_colorid: color };
+      let productColorJson = JSON.stringify(co);
+
+      fetch(`${this.addProductColors}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: productColorJson,
+      });
+    },
+    uploadImage: function (event) {
+      var input = event.target;
+      if (input.files) {
+        var reader = new FileReader();
+        this.image = input.files[0];
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
     async fetchBrand() {
       const res = await fetch(this.brandUrl);
       const data = await res.json();
