2626948d54b96e691675a8ac1d3a400b9a885305
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-05-11 00:52:01 +0700

    [Update] Team Page Responsive

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="team grid grid-cols-3 grid-rows-1 gap-4 mx-20 my-10">
+  <div class="xl:grid xl:grid-cols-3 xl:grid-rows-1 xl:gap-4 xl:mx-20 xl:my-10 md:gap-y-5 md:my-5 my-4 gap-y-4">
     <div class="text-center mx-auto">
-      <img src="../assets/sitkmutt.png" class="mx-auto w-72">
-      <img src="../assets/mew.png" class="rounded-full my-2 w-72 mx-auto" style="filter:brightness(90%);">
+      <img src="../assets/sitkmutt.png" class="mx-auto xl:w-72 md:w-72 w-48">
+      <img src="../assets/mew.png" class="rounded-full my-2 xl:w-72 md:w-72 w-48 mx-auto" style="filter:brightness(90%);">
       <div class="bg-orange text-white rounded-xl w-max mx-auto py-1 px-2">62130500056</div>
       <p>PIYATHIDA THANEEWAN</p>
       <p class="text-reds text-sm">RESPONSIBILITY: BACK-END & DATABASE</p>
     </div>
     <div class="text-center mx-auto">
-      <img src="../assets/sitkmutt.png" class="mx-auto w-72">
-      <img src="../assets/sk.png" class="rounded-full my-2 w-72 mx-auto"  style="filter:brightness(70%);">
+      <img src="../assets/sitkmutt.png" class="mx-auto xl:w-72 xl:visible md:invisible invisible w-48">
+      <img src="../assets/sk.png" class="rounded-full my-2 xl:w-72 md:w-72 mx-auto w-48"  style="filter:brightness(70%);">
       <div class="bg-orange text-white rounded-xl w-max mx-auto py-1 px-2">62130500060</div>
       <p>PONGPICHET TANTHANAWIKRAI</p>
       <p class="text-reds text-sm">RESPONSIBILITY: FRONT-END & DEVOPS</p>
     </div>
     <div class="text-center mx-auto">
-      <img src="../assets/sitkmutt.png" class="mx-auto w-72">
-      <img src="../assets/win.jpg" class="rounded-full my-2 w-72 mx-auto"  style="filter:brightness(120%);">
+      <img src="../assets/sitkmutt.png" class="mx-auto xl:w-72 xl:visible md:w-72 md:invisible invisible w-48">
+      <img src="../assets/win.jpg" class="rounded-full my-2 xl:w-72 md:w-72 mx-auto w-48"  style="filter:brightness(120%);">
       <div class="bg-orange text-white rounded-xl w-max mx-auto py-1 px-2">62130500064</div>
       <p>POSSAWAT TANAM</p>
       <p class="text-reds text-sm">RESPONSIBILITY: BACK-END & DEVOPS</p>
